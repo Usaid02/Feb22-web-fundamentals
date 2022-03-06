381ea32e05402aadd6f65325aca2674ab81c47ca
Mohd Used <91274057+Usaid02@users.noreply.github.com>
2022-03-06 17:56:46 +0530

    Create SumOfNumber.java

---
+++ february/week2/Assignment3/SumOfNumber.java
@@ -0,0 +1,16 @@
+package febweekfirst;
+                                            // Calculate sum of numbers
+            import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=1; i<=n; ++i){
+            sum += i;
+            System.out.println(sum);
+        }
+
+    }
+}
