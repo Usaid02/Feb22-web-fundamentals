0d65041b6dd7cbcf8f98d8c87480d7a27dc39435
Mohd Used <91274057+Usaid02@users.noreply.github.com>
2022-03-06 21:20:51 +0530

    Create FectorialOfIntDec.java

---
+++ february/week3rd/Assignment2/FectorialOfIntDec.java
@@ -0,0 +1,13 @@
+package weekthird;
+import java.util.Scanner;
+public class FactorialOfIntDec {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input integer: ");
+        int a = sc.nextInt();
+        System.out.print("Input decimal: ");
+        float b = sc.nextFloat();
+        float product = a * b;
+        System.out.println(product);
+    }
+}
