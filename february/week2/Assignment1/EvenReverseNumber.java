31a4d83a8108d8a5c38358ff3cb9c8e5ed31ab59
Mohd Used <91274057+Usaid02@users.noreply.github.com>
2022-03-06 17:52:47 +0530

    Add files via upload

---
+++ february/week2/Assignment1/EvenReverseNumber.java
@@ -0,0 +1,12 @@
+package febweekfirst;
+//                                print even reverse number
+public class EvenReverseNumber {
+    public static void main(String args[]){
+        int i = 10;
+        i = 10;
+        while(i>=2){
+            System.out.print(i + " ");
+            i = i-2;
+        }
+    }
+}
---
+++ february/week2/Assignment1/ReverseNumber.java
@@ -0,0 +1,10 @@
+package febweekfirst;
+//                                print reverse even number
+public class ReverseNumber {
+    public static void main(String args[]){
+        int i = 10;
+        for(i=10; i>=2; i=i-2){
+            System.out.print(i + " ");
+        }
+    }
+}
