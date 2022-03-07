0f108cb194a398d3231ea651ef5ca8e3f8d3dbfa
Mohd Used <91274057+Usaid02@users.noreply.github.com>
2022-03-07 09:54:01 +0530

    Update and rename FectorialOfIntDec.java to ProductOfIntDec.java

--- february/week3rd/Assignment2/ProductOfIntDec.java
+++ february/week3rd/Assignment2/ProductOfIntDec.java
@@ -1,6 +1,6 @@
 package weekthird;
 import java.util.Scanner;
-public class FactorialOfIntDec {
+public class ProductOfIntDec {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input integer: ");
